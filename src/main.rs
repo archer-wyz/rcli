0ea963edd1bf1ade27c2ba172db791361e71ac65
wanyaozhong <wanyaozhong2021@163.com>
2024-04-19 15:51:07 +0800

    Add 增加 csv 的功能

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Hello world!2");
+use ::anyhow;
+use ::clap::Parser;
+use rcli::{process_csv, Opts, SubCommand};
+
+fn main() -> anyhow::Result<()> {
+    let opts: Opts = Opts::parse();
+    match opts.cmd {
+        SubCommand::Csv(csv_opts) => process_csv(&csv_opts.input, &csv_opts.output)?,
+    }
+    Ok(())
 }
