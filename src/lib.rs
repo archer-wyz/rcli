8013b8857b3dc7e04f05c66f5edf1f4b582025c5
wanyaozhong <wanyaozhong2021@163.com>
2024-04-19 19:04:51 +0800

    Add 增加 gen_pass 的功能

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,5 @@ mod opts;
 mod process;
 
 pub use opts::{CsvOpts, Opts, SubCommand};
-pub use process::process_csv;
+pub use process::csv_convert::process_csv;
+pub use process::gen_pass::process_gen_pass;
---
+++ src/process/gen_pass.rs
@@ -0,0 +1,36 @@
+use ::rand;
+use anyhow::Result;
+use rand::prelude::IndexedRandom;
+
+pub fn process_gen_pass(
+    length: usize,
+    count: usize,
+    uppercase: bool,
+    lowercase: bool,
+    number: bool,
+    symbol: bool,
+) -> Result<()> {
+    let mut rng = rand::thread_rng();
+    let mut charset = Vec::new();
+    if uppercase {
+        charset.extend_from_slice(b"ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    if lowercase {
+        charset.extend_from_slice(b"abcdefghijklmnopqrstuvwxyz");
+    }
+    if number {
+        charset.extend_from_slice(b"0123456789");
+    }
+    if symbol {
+        charset.extend_from_slice(b"!@#$%^&*()-_=+");
+    }
+    let mut password = String::with_capacity(length);
+    for _ in 0..count {
+        for _ in 0..length {
+            let c = charset.choose(&mut rng).expect("chars want be empty");
+            password.push(*c as char);
+        }
+        println!("{}", password);
+    }
+    Ok(())
+}
